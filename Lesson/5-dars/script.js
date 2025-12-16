133d3b23a912014dfef5492421e0c2793d9405e5
Abdulatif <abdulatifa801@gmail.com>
2025-12-16 20:30:35 +0500

    add 5-dars

---
+++ Lesson/5-dars/script.js
@@ -0,0 +1,19 @@
+let baho = prompt(' Bahoni kiriting')
+
+
+if (baho === "A") {
+    console.log("a'lo");
+    
+}else if (baho === "B"){
+    console.log('Yaxshi');
+    
+}else if (baho === "C"){
+    console.log('qoniqarli');
+    
+}else if (baho === "F"){
+    console.log('Yomon');
+    
+}else {
+  console.log("Noto‘g‘ri baho");
+}
+
