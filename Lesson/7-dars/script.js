080fee914e4fcccb4a9ffef41ee1bd801851d30e
Abdulatif <abdulatifa801@gmail.com>
2025-12-17 19:48:46 +0500

    add 8-dars

---
+++ Lesson/7-dars/script.js
@@ -0,0 +1,15 @@
+let fruits = ['olma', 'banan', 'aplesing']
+// console.log(fruits);
+
+// fruits.push('anor')
+// console.log(fruits);
+
+// fruits.pop()
+// console.log(fruits);
+
+
+// fruits.unshift("kiwi");
+// console.log(fruits); 
+
+// console.log(fruits.length); 
+// 3
