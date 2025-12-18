d6744d975008d6d2f846a2c7f142b03636ae36d1
Abdulatif <abdulatifa801@gmail.com>
2025-12-18 20:26:08 +0500

    add 10-dars

---
+++ Lesson/10-dars/script.js
@@ -0,0 +1,12 @@
+let talaba = {
+    ism: "Ali", 
+    yosh: 20,
+    kurs: 3,
+    fanlar: ["Matematika", "Fizika", "Ingliz tili"],
+    Salom : function() {
+        console.log("Salom, mening ismim " + this.ism);
+    }
+
+
+}
+console.log(talaba);
